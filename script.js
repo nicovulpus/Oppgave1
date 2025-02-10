4b29a2c5d2327b9ea3127492f34a2a513ff50286
nicovulpus <nicholas.v.vandremo@gmail.com>
2025-02-10 13:07:09 +0100

    Bare minimum av oppgaven lagt til

---
+++ script.js
@@ -0,0 +1,21 @@
+let string1 = "Dette er den første stringen!";
+let string2 = "Dette er den andre stringen!";
+const string3 = "En litt crazy string på slutten. Whoa!";
+
+let integer1 = 20;
+let integer2 = 30;
+
+const person = {
+  name: "Charlie",
+  age: 32,
+  hair: "red",
+};
+
+const dog = {
+  type: "German Shepard",
+  height: 60,
+  weight: 20,
+};
+
+let frukt = ["eple", "banan", "ananas", "pære"];
+let spill = ["Elden Ring", "BG3", "Detroit: Human", "Dark Souls"];
